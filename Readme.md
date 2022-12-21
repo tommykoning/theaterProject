30596ff03efc4d1c27bad9d164236573cf55ea3c
tommy <tommy@TOMMY.local>
2022-12-21 22:52:32 +0100

    Update .gitignore, appsettings.Development.json, and 60 more files...: het project docker competible gemaakt

--- Readme.md
+++ Readme.md
@@ -6,4 +6,3 @@ clone the repo
 open terminal at the project root and run these commands to install and run:
 
 > docker-compose up --build -d
-
---
+++ server/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// enable if you want t use HTTPS, remember to change the Proxy in the client.
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
