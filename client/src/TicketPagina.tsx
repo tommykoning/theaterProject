cafb93b7a1d3a2d1159da605336e83940a807e70
Msa-786 <94054037+Msa-786@users.noreply.github.com>
2023-01-09 18:25:20 +0100

    Update package.json and TicketPagina.tsx

---
+++ client/src/TicketPagina.tsx
@@ -0,0 +1,17 @@
+import {useState, useEffect} from 'react';
+
+const header = () => {
+  return (
+    <header>
+      <nav className="topNav">
+        <ul>
+          <li><a href="#">dummy1</a></li>
+          <li><a href="#">dummy2</a></li>
+          <li><a href="#">dummy3</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default header;
